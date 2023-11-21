321d3e894ef97e27972c16cf5bacff02097ed848
TameezDeen <80967823+TameezDeen@users.noreply.github.com>
2023-11-22 00:39:57 +0530

    showing song based on language preference

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -2,39 +2,42 @@ const fs = require('fs');
 const csvParser = require('csv-parser');
 const _ = require('lodash');
 
-const filterSongsByGenres = (selectedGenres) => {
-  return new Promise((resolve, reject) => {
-      const groupedSongs = {};
+const filterSongsByGenres = (selectedGenres, languagePreference) => {
+    return new Promise((resolve, reject) => {
+        const groupedSongs = {};
 
-      fs.createReadStream('assets/dataset.csv')
-          .pipe(csvParser())
-          .on('data', (row) => {
-              if (selectedGenres.includes(row.genre)) {
-                  if (!groupedSongs[row.genre]) {
-                      groupedSongs[row.genre] = [];
-                  }
-                  groupedSongs[row.genre].push({
-                      track_id: row.track_id,
-                      track_name: row.track_name,
-                      artists: row.artists,
-                      genre: row.genre
-                  });
-              }
-          })
-          .on('end', () => {
-              const randomSongs = {};
+        fs.createReadStream('assets/dataset.csv')
+            .pipe(csvParser())
+            .on('data', (row) => {
+                if (
+                    selectedGenres.includes(row.genre) &&
+                    row.language === languagePreference
+                ) {
+                    if (!groupedSongs[row.genre]) {
+                        groupedSongs[row.genre] = [];
+                    }
+                    groupedSongs[row.genre].push({
+                        track_id: row.track_id,
+                        track_name: row.track_name,
+                        artists: row.artists,
+                        genre: row.genre,
+                    });
+                }
+            })
+            .on('end', () => {
+                const randomSongs = {};
 
-              // Select random 5 songs from each genre
-              Object.keys(groupedSongs).forEach((genre) => {
-                  randomSongs[genre] = _.sampleSize(groupedSongs[genre], 5);
-              });
+                // Select random 5 songs from each genre
+                Object.keys(groupedSongs).forEach((genre) => {
+                    randomSongs[genre] = _.sampleSize(groupedSongs[genre], 5);
+                });
 
-              resolve(randomSongs);
-          })
-          .on('error', (error) => {
-              reject(error);
-          });
-  });
+                resolve(randomSongs);
+            })
+            .on('error', (error) => {
+                reject(error);
+            });
+    });
 };
 
 module.exports = { filterSongsByGenres }
--- backend/routes/song.js
+++ backend/routes/song.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const requireAuth = require('../middleware/requireAuth');
 const { filterSongsByGenres } = require('../controllers/songController');
+const User  = require('../models/userModel');
 
-router.post('/filtered-songs', async (req, res) => {
+router.post('/filtered-songs', requireAuth, async (req, res) => {
   const { selectedGenres } = req.body;
   try {
-    const filteredSongs = await filterSongsByGenres(selectedGenres);
+    // Check if req.user is defined
+    if (!req.user || !req.user._id) {
+        return res.status(401).json({ error: 'User not authenticated' });
+    }
+
+    // Fetching user details to get the language preference
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+    }
+
+    const languagePreference = user.languagePreference;
+
+    // Call the function with both selected genres and language preference
+    const filteredSongs = await filterSongsByGenres(selectedGenres, languagePreference);
     res.json({ songs: filteredSongs });
-  } catch (error) {
+} catch (error) {
     console.error('Error filtering songs:', error);
     res.status(500).json({ error: 'Internal Server Error' });
-  }
+}
 });
 
 module.exports = router;
